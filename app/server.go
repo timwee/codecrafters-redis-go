75a6b187a41933c04f26833a31c7d1152ace1bd1
Tim Wee <tim.shawn@gmail.com>
2023-11-11 00:32:05 -0500

    <your_commit_message>

--- app/server.go
+++ app/server.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strings"
 	// "os"
+)
+
+const (
+	PING        = "*1\r\n$4\r\nping\r\n"
+	ECHO_PREFIX = "*2\r\n$4\r\nECHO\r\n$"
+	SEP         = "\r\n"
+	PONG        = "+PONG\r\n"
 )
 
 func main() {
@@ -26,13 +36,42 @@ func main() {
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 	for {
-		buf := make([]byte, 1024)
-		_, err := conn.Read(buf)
+		var buf bytes.Buffer
+		tmp := make([]byte, 1024)
+		n, err := conn.Read(tmp)
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println(err)
+				return
+			}
+		}
+		buf.Write(tmp[:n])
+
+		respond(buf, conn)
+	}
+
+}
+
+func respond(buf bytes.Buffer, conn net.Conn) {
+	toks := strings.Split(buf.String(), SEP)
+	if isPing(toks) {
+		conn.Write([]byte(PONG))
+	} else if isEcho(toks) {
+		fmt.Printf("in echo, echoing back %s", toks[4])
+		_, err := fmt.Fprintf(conn, "$%d\r\n%s\r\n", len(toks[4]), toks[4])
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		conn.Write([]byte("+PONG\r\n"))
+	} else {
+		// fmt.Printf("unsupported command: %s", buf.String())
 	}
+}
 
+func isEcho(toks []string) bool {
+	return len(toks) >= 5 && strings.Compare(toks[2], "echo") == 0
+}
+
+func isPing(toks []string) bool {
+	return len(toks) >= 3 && strings.Compare(toks[2], "ping") == 0
 }
